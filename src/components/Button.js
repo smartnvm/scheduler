c9408c377dc5ac1a827e8f482ed8da0468ca41ff
AJ <aj@smartnvm.com>
2021-10-28 02:02:26 -0400

    - add button component - test button styling bheaviour with storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+  const { confirm, danger, disabled } = props;
+  console.log(props);
+  //base button class
+  let buttonClass = 'button';
+
+  if (confirm) {
+    buttonClass += ' button--confirm';
+  }
+  if (danger) {
+    buttonClass += ' button--danger';
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
