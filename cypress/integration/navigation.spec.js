2e5d3082188e218044341dad21be1f1e7717b01f
AJ <aj@smartnvm.com>
2021-11-04 03:21:37 -0400

    - add cypress / NODE_ENV=test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,9 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+    cy.contains("li", "Tuesday")
+    .click()
+    .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+  });
+});
